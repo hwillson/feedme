66d8406abcd18e86020212d3a6c364da61a69cb5
hwillson <hugh@octonary.com>
2015-01-23 06:58:15 -0500

    Wired up feed delete.

--- client/views/feeds/feed.js
+++ client/views/feeds/feed.js
@@ -2,6 +2,13 @@ Template.feed.events({
 
 	'click .js-edit-feed': function (e) {
 		Session.set('selectedFeedId', this._id);
-	}
+	},
+
+	'click .js-delete-feed': function (e) {
+    e.preventDefault();
+    if (confirm('Are you sure you want to delete this feed?')) {
+      Feeds.remove(this._id);
+    }
+  }
 
 });
