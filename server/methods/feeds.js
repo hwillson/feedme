140bd63c93b444388386a5ef468e3e6b600b0f2f
hwillson <hugh@octonary.com>
2015-02-01 09:27:13 -0500

    Prepped app to work with local fork of the danielqiu:feed package.

--- server/methods/feeds.js
+++ server/methods/feeds.js
@@ -1,9 +1,7 @@
 Meteor.methods({
 
   refreshFeeds: function () {
-    // TODO - get custom stopReading working in danielqiu:feed fork and
-    // enable when ready
-    // Feed.stopReading();
+    Feed.stopReading();
     Feed.read();
   }
 
