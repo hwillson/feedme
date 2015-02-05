a7d5eed0eccb9e6e26e46499979ed59d6d82d8bd
hwillson <hugh@octonary.com>
2015-02-05 10:32:02 -0500

    Wired up public welcome page and adjusted non-authenticated header.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -2,6 +2,14 @@ Router.configure({
 	layoutTemplate: 'layout',
 });
 
+Router.onBeforeAction(function () {
+  if (!Meteor.userId()) {
+    this.render('welcome');
+  } else {
+    this.next();
+  }
+});
+
 // Show all feed entries by default.
 Router.route('/', function () {
   this.render('feedEntries');
